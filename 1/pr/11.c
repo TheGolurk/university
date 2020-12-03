f6b342e1193cfd709e56f3699bc51d14fc218c7c
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-12-03 11:47:17 -0600

    git conflict

--- 1/pr/11.c
+++ 1/pr/11.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-
 int main() {
 
 	int x = 0;
@@ -15,20 +13,6 @@ int main() {
 		++x;
 
 	} while(x <= 10);
-=======
-int main(int argc, char **argv) {
-
-	int number, number2;
-	printf("Number of iterations ");
-	scanf("%d", &number);
-
-	printf("Number of multiplications ");
-	scanf("%d", &number2);
-
-	for (int i = 1; i <= number; i++) {
-		printf("%d x %d = %d \n", number2, i, number2 * i);
-	}
->>>>>>> 071b89bd714db213c92b2d2265608c3a617e5b3d
 
 	return 0;
 }
