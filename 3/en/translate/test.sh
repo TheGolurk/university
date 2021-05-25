4f0961af128555a7d6bb001662df4f261a2f60f5
Christian Hernandez <chrismatla68@gmail.com>
2021-05-25 14:21:05 -0500

    Fixed a little bug...

--- 3/en/translate/test.sh
+++ 3/en/translate/test.sh
@@ -5,4 +5,4 @@ START_TIME=$(date +%s)
 go run main.go es en prueba
 
 END_TIME=$(date +%s)
-echo "It took $(($END_TIME - $START_TIME)) to translate..."
+echo "It took $(($END_TIME - $START_TIME)) Seconds to translate..."
