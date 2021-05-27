97e27b457ea0d741009f2e798eecc629001b900e
Christian <chrismatla68@gmail.com>
2021-05-27 17:19:23 -0500

    totally more readable

--- 3/c/mierdas_eps/3.c
+++ 3/c/mierdas_eps/3.c
@@ -2,6 +2,32 @@
 
 int main() {
 
+  int nums = 1;
+  int space = 10;
+
+  for (size_t t = 0; t < 10; t++)
+  { 
+
+    for (size_t i = 0; i < space; i++)
+    {
+      printf(" ");
+    }
+
+    for (size_t i = 0; i < nums; i++)
+    {
+      printf("%d",i);
+    }
+
+    for (size_t i = 1; i < nums; i++)
+    {
+      printf("%d",i);
+    }
+
+    nums++;
+    space--;
+    printf("\n");
+
+  }
 
   return 0;
 }
