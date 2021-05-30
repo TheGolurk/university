ddea9cb21f1d890ab2e7ca99ac79944c1bfa40d9
Christian <chrismatla68@gmail.com>
2021-05-29 21:06:08 -0500

    grmbl

--- 3/c/eps/EP1/4.c
+++ 3/c/eps/EP1/4.c
@@ -7,9 +7,24 @@ Analisis:
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+const char * Draw(int lenght, int size);
+
+void handlerErr(int number) {
+    fprintf(stderr, "you have entered a wrong number: %d \n", number);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char const *argv[]) {
 
 
   return 0;
+}
+
+const char * Draw(int lenght, int size) {
+
+  char * picture;
+
+  return picture;
 }
