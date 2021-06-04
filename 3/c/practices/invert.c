feb2260c1fd195b0b7f1376ada9ba41a4af5b6ec
Christian <chrismatla68@gmail.com>
2021-06-04 15:19:12 -0500

    and so the crazy refactoring process sees the sunlight after some months in the dark!

--- 3/c/practices/invert.c
+++ 3/c/practices/invert.c
@@ -6,17 +6,9 @@ int * invert(int arr[], int size);
 
 int main(int argc, char const *argv[])
 {
-    
     int values[] = {1,2,3,4,5};
-    int tmp[NELEMS(values)-1];
     int val = NELEMS(values)-1;
     int *new = invert(values, val);
-
-    for (size_t i = 0; i <= val; i++)
-    {
-        tmp[i] = values[val-i];
-        printf("[%d]: %d %d \n", i, tmp[i], new[i]);
-    }
 
     return 0;
 }
@@ -25,6 +17,7 @@ int * invert(int arr[], int size) {
     for (size_t i = 0; i <= size; i++)
     {
         arr[i] = arr[size-i];
+        printf("[%d]: %d", i, arr[i]);
     }
     return arr;
 }
