d71bebce85183ab825975baa652f99fbec8c6582
Christian Hernandez <chrismatla68@gmail.com>
2021-07-02 18:43:51 +0000

    asdasdad

--- 3/c/eps/ep2/main.c
+++ 3/c/eps/ep2/main.c
@@ -3,17 +3,26 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 #define MAX_SIZE 3000
+
+#define GET_SIZE(x)  (sizeof(x) / sizeof((x)[0]))
 
 int main(int argc, char const *argv[])
 {
     int opt;
 
-    while(opt != 4) {
+    char* students[MAX_SIZE];
+    int dates[MAX_SIZE];
+    // bool asistence[MAX_SIZE];
 
-      
+    students[0] = "LORENA VALLE";
+    students[1] = "LORENA VALLE 2222";
+  
+    printf("%s \t %s \n", students[0], students[1]);
+
+    while(opt != 4) {
 
       printf(" 1.- Registrar Alumno \n 2.- Registrar asistencia \n 3.- Desplegar informacion \n 4.-Salir\n");
       scanf("%d", &opt);
