6fc1e957a090ac44832dbf90b94f51a4c8078b61
Christian Hernandez <chrismatla68@gmail.com>
2021-07-02 18:15:16 +0000

    sxcSdcsadad

--- 3/c/eps/ep2/main.c
+++ 3/c/eps/ep2/main.c
@@ -3,11 +3,21 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_SIZE 3000
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int opt;
+
+    while(opt != 4) {
+
+      
+
+      printf(" 1.- Registrar Alumno \n 2.- Registrar asistencia \n 3.- Desplegar informacion \n 4.-Salir\n");
+      scanf("%d", &opt);
+    }
+
     return 0;
 }
