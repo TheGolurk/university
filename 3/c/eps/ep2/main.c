6abc35e0e8ee5100e54c41c75ed9e2a3baa4f26f
Christian Hernandez <chrismatla68@gmail.com>
2021-07-04 01:25:21 +0000

    holasdajkdhsjkdhajkdasa

--- 3/c/eps/ep2/main.c
+++ 3/c/eps/ep2/main.c
@@ -29,17 +29,45 @@ int main(int argc, char const *argv[])
 
     char* students[MAX_SIZE];
     int dates[MAX_SIZE];
-    // bool asistence[MAX_SIZE];
+    char* asistence[MAX_SIZE];
 
-    students[0] = "LORENA VALLE";
-    students[1] = "LORENA VALLE 2222";
-    students[2] = "CHRISTIAN";
-    pass_asistence(students, MAX_SIZE);
+    students[0] = "Lorena";
+    dates[0] = 03072021;
+    asistence[0] = "si";
+
+    students[1] = "Luis";
+    dates[1] = 31122021;
+    asistence[1] = "no";
+
+    
 
     while(opt != 4) {
 
-      printf(" 1.- Registrar Alumno \n 2.- Registrar asistencia \n 3.- Desplegar informacion \n 4.-Salir\n");
+      printf(" 1.- Registrar Alumno \n 2.- Registrar fecha de asistencia \n 3.- Desplegar informacion \n 4.-Salir\n");
       scanf("%d", &opt);
+
+
+      switch(opt){
+
+        case 1:
+        break;
+
+        case 2:
+        break;
+
+        case 3:
+          pass_asistence(students, MAX_SIZE);
+          break;
+        
+        case 4:
+        break;
+
+        default: 
+          printf("Incorrect option\n");
+          return 0;
+      }
+
+
     }
 
     return 0;
