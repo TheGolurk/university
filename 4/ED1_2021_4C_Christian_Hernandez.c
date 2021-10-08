90e24061a1508cb2a1459c712e5480c5dcdb7469
Christian Hernandez Najera <christian.najera@rappi.com>
2021-10-08 12:51:51 -0500

    omg what have I done?

--- 4/ED1_2021_4C_Christian_Hernandez.c
+++ 4/ED1_2021_4C_Christian_Hernandez.c
@@ -6,10 +6,26 @@ ED1
 08/10/2021
 */
 
+
 #include <stdio.h>
+
+struct Word {
+    char Letter;
+    struct Word *next;
+};
+
+struct Employee {
+    char Name[100];
+    char CURP[18];
+}; 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int opt;
+
+    printf("Introduce an option: \n");
+    printf("1.- Insert employee \n 2.- Test Palindrome word \n 3.- Show Employees \n");
+    scanf("%d", opt);
+    
     return 0;
 }
