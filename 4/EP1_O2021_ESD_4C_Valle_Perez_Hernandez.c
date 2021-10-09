ab635bef278befe3878fd63ec45ac12e55fd7659
Christian Hernandez Najera <christian.najera@rappi.com>
2021-10-09 17:34:42 -0500

    foo

--- 4/EP1_O2021_ESD_4C_Valle_Perez_Hernandez.c
+++ 4/EP1_O2021_ESD_4C_Valle_Perez_Hernandez.c
@@ -1,17 +1,32 @@
+/*
+EQUIPO: Valle Gonzalez Lorena
+        Perez Baltazar Mitzi Guadalupe
+        Hernandez Najera Christian
+
+GRADO Y GRUPO: 4.C ITI
+
+REPOSITORIO: https://github.com/TheGolurk/university/blob/master/4/EP1_O2021_ESD_4C_Valle_Perez_Hernandez.c
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 
 // struct
 struct Register {
-    char  carplates;
-    char  model;
-    char  color;
+    char  carplates[30];
+    char  model[30];
+    char  color[30];
     int   date;
     float checkin;
     float checkout;
     char  name[30];
-    struct Register next;
+    
 };
 
+struct queue{
+    struct Register data;
+    struct queue *next;
+};
 
 // Links a recursos que nos puede ayudar a hacer esta cosa
 // https://gist.github.com/rdleon/d569a219c6144c4dfc04366fd6298554
@@ -35,7 +50,10 @@ int main(int argc, char const *argv[])
 
 void Menu(){
     int opt;
+    printf("Ingresa una opcion \n");
+    printf(" 1.- Registrar entrada \n 2.- Registrar Salida \n 3.- Mostrar \n 4.- Salir \n");
     scanf("%d", &opt);
+
 
     while (opt != 4)
     {
@@ -43,6 +61,15 @@ void Menu(){
         switch (opt)
         {
         case 1:
+            break;
+
+        case 2:
+            break;
+        
+        case 3:
+            break;
+            
+        case 4:
             break;
         
         default:
@@ -53,3 +80,14 @@ void Menu(){
 
 }
 
+void check_in() {
+
+}
+
+void check_out() {
+
+}
+
+void show() {
+
+}
