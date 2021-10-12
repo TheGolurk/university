2727ebe3b56b6bbb6d489a2f1f01806de57a5848
Christian Hernandez Najera <christian.najera@rappi.com>
2021-10-12 12:57:26 -0500

    I must enjoy torturing myself

--- 4/tree_class.c
+++ 4/tree_class.c
@@ -1,3 +1,21 @@
+/*
+Recorridos
+-Profundidad
+	-Pre orden
+		-visitar raiz
+		- recorrer en preorden sub arbol izquierdo
+		- recorrer en preorden sub arbol derecho 
+	-In orden
+		- recorrer en in orden sub arbol izquierdo
+		- visitar raiz
+		- recorrer en in orden sub arbol derecho
+	-Post Orden
+		-recorrer en post orden sub arbol izq
+		-recorrer en post orden sub arbol der
+		-visitar raiz
+-Amplitud
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,6 +26,7 @@ struct Nodo {
 };
 
 int insertar(int nuevoDato, struct Nodo **raiz);
+void inOrden(struct Nodo *raiz);
 
 int main() {
 	struct Nodo *raiz = NULL;
@@ -16,6 +35,8 @@ int main() {
 	insertar(9, &raiz);
 	insertar(10, &raiz);
 	insertar(3, &raiz);
+
+	inOrden(raiz);
 
 	return 0;
 }
@@ -43,6 +64,7 @@ int insertar(int nuevoDato, struct Nodo **raiz) {
 	{
 		return insertar(nuevoDato, &((*raiz)->izq));
 	} else {
+		// quitamos el if si quieremos repetidos
 		if (nuevoDato > (*raiz)->dato)
 		{
 			return insertar(nuevoDato,  &((*raiz)->der));
@@ -53,4 +75,14 @@ int insertar(int nuevoDato, struct Nodo **raiz) {
 	}
 	
 
+}
+
+void inOrden(struct Nodo *raiz) {
+	if (raiz == NULL) {
+		return;
+	}
+
+	inOrden(raiz->izq);
+	printf("%d, ", raiz->dato);
+	inOrden(raiz->der);
 }
