92aa496fdd7acbfadbe4c77514b0c94659a034cb
Christian Hernandez Najera <chrismatla68@gmail.com>
2021-11-06 16:40:19 -0600

    some brief changes

---
+++ 4/estancia/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Running..."
+
+sleep 1
+
+gcc -c -Wall -Werror -Wextra *.c
+
+ar -rc libname.a *.o
+
+ranlib libname.a
+
+ar -rcs libname.a *.o
+
+gcc proyecto.c -L. -lname -o a.out
+
+./a.out && rm *.o *.a *.out
--- 4/estancia/validator.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VALIDATOR_H
-#define _VALIDATOR_H
-
-#define STUDENT_ID 1000
-#define ADMIN_ID 9999
-
-int validar_usuario(int ID);
-
-#endif
