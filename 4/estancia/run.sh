961b138e36b0713966c131cbc1176e0ecc386d9a
Christian Hernandez Najera <chrismatla68@gmail.com>
2021-11-21 16:33:57 -0600

    Oh my god what year is it?!

--- 4/estancia/run.sh
+++ 4/estancia/run.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-echo "Running..."
-
-sleep 1
-
-cd src
-
-gcc -c -Wall -Werror -Wextra *.c
-
-ar -rc libname.a *.o
-
-ranlib libname.a
-
-ar -rcs libname.a *.o
-
-gcc proyecto.c -L. -lname -o a.out
-
-./a.out && rm *.o *.a *.out
+gcc proyecto.c -o a.out && ./a.out
