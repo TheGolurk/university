b6061357546fc6ac4527ecd4669b687c2e7c9773
Christian Hernandez Najera <chrismatla68@gmail.com>
2021-11-23 21:10:38 -0600

    hey, what's that over there?!

---
+++ 4/ep3/nuevo.c
@@ -0,0 +1,193 @@
+/*
+EP3 ALGORITMOS DE BUSQUEDA Y ORDENAMIENTO 
+
+AUTORES:
+VALLE GONZALEZ LORENA
+HERNANDEZ NAJERA CHRISTIAN
+
+*/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+int* generarInstancia(int tam, int min, int max);
+int* generarDatosBuscadosExistentes(int tam, int porcentaje, int* datos);
+int* generarDatosBuscadosNE(int tam, int min, int max, int porcentaje);
+int* obtenerDatos(int tam, int porcentaje, int* datos);
+
+int buscar(int* arr, int l, int r, int x);
+
+double obtener_tiempo();
+
+void busquedaBinaria(int *datos,int tam, int *datosB, int logs, int tamb);
+void mostrar_informacion(int encontrado, int dato, int logs);
+void mostrar(int tam, int* datos);
+
+
+int main ()
+{
+    int tam, tamB, min, max, porcentaje;
+    int *datos, *datosb;  
+    int opc=1;
+    int opc2;
+
+  while(opc != 2)
+    {
+        printf("ingrese una opcion: \n1.-Correr prueba \n2.-Salir\n");
+        scanf("%d", &opc);
+
+        switch(opc)
+        {
+            case 1:
+                printf("Ingresa el tamaño:\n");
+                scanf("%d", &tam);
+
+                printf("Ingresa el porcentaje: %(10,50,100)\n");
+                scanf("%d", &porcentaje);
+
+                printf("Ingrese minimo para generar los numeros\n");
+                scanf("%d", &min);
+
+                printf("Ingrese maximo\n");
+                scanf("%d", &max);
+
+                datos = generarInstancia(tam, min, max);
+                mostrar(tam, datos);
+
+                break;
+
+            case 2:
+                printf("Saliendo del programa.......\n");
+                break;
+
+            default:
+                printf("Opcion incorrecta\n");
+                break;
+
+        }
+
+    }
+
+
+    return 0;
+}
+
+int* obtenerDatos(int tam, int porcentaje, int* datos){
+    int tamNuevo;
+
+    switch (porcentaje)
+    {
+    case 10:
+        tamNuevo = (tam*0.10) - 1;
+        break;
+    
+    case 50:
+        tamNuevo = (tam / 2) - 1;
+        break;
+
+    case 100:
+        tamNuevo = tam -1;
+        break;
+
+    default:
+        printf("Porcentaje incorrecto");
+        return datos;
+        break;
+    }
+
+    int* datosb=(int *)malloc(sizeof(int)*tamNuevo);
+
+    for (size_t i = 0; i < tamNuevo; i++)
+    {
+        datosb[i] = datos[i];
+    }
+    
+    return datosb;
+}
+    
+
+void mostrar(int tam, int* datos){
+    printf("\n");
+
+    for(size_t i=0; i<tam; i++){
+        printf("%d, ", datos[i]);
+        
+    }
+    printf("\n");
+}
+
+int* generarInstancia(int tam, int min, int max){
+    int* datos=(int *)malloc(sizeof(int)*tam);
+
+    for(size_t i=0; i<tam; i++){
+        datos[i] = rand()%(max-min+1)+min;
+    }
+    
+    return datos;
+}
+
+int* generarDatosBuscadosExistentes(int tam, int porcentaje, int* datos){
+    int *datosB, tamB, i;
+    tamB=tam*porcentaje/100;
+    
+    datosB=(int *)malloc(sizeof(int)*tamB);
+
+    for(i=0; i<tamB; i++){
+        datosB[i]=datos[rand()%tam]; 
+    }
+    
+    return datosB;
+}
+
+int* generarDatosBuscadosNE(int tam, int min, int max, int porcentaje){
+    int *datosB, tamB, i;
+    tamB=tam*porcentaje/100;
+    
+    datosB=(int *)malloc(sizeof(int)*tamB);
+
+    for(i=0;  i<tamB;i++){
+        datosB[i]=rand()%(max-min+1)+min; 
+    }
+
+    return datosB;
+}
+
+int buscar(int* arr, int l, int r, int x) {
+    if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+        if (arr[mid] == x) 
+            return mid; 
+        if (arr[mid] > x) 
+            return buscar(arr, l, mid - 1, x); 
+        return buscar(arr, mid + 1, r, x); 
+    } 
+    return -1; 
+}
+
+void busquedaBinaria(int *datos,int tam, int *datosB, int logs, int tamb) {
+    for (size_t i = 0; i < tamb; i++)
+    {
+        int encontrado =  buscar(datos, 0, tam, datosB[i]);
+        mostrar_informacion(encontrado, datosB[i], logs); 
+    }
+    
+}
+
+void mostrar_informacion(int encontrado, int dato, int logs) {
+    if (logs == 1)
+    {
+        if (encontrado == -1) {
+            printf("%d no encontrado\n", dato);
+        } else {
+            printf("%d encontrado\n", dato);
+        }
+    }
+} 
+
+double obtener_tiempo() {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return t.tv_sec + t.tv_usec/10000.000;
+}
