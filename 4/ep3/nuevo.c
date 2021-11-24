d3c785c6832d42702bbffb86a45761d6c08f8750
Christian Hernandez Najera <chrismatla68@gmail.com>
2021-11-23 22:11:07 -0600

    bumping poms

--- 4/ep3/nuevo.c
+++ 4/ep3/nuevo.c
@@ -177,21 +177,22 @@ int* generarDatosBuscadosNE(int tam, int min, int max, int porcentaje){
 }
 
 int buscar(int* arr, int l, int r, int x) {
-    if (r >= l) { 
-        int mid = l + (r - l) / 2; 
-        if (arr[mid] == x) 
-            return mid; 
-        if (arr[mid] > x) 
-            return buscar(arr, l, mid - 1, x); 
-        return buscar(arr, mid + 1, r, x); 
-    } 
+    while (l <= r) { 
+        int m = l + (r - l) / 2; 
+        if (arr[m] == x) 
+            return m; 
+        if (arr[m] < x) 
+            l = m + 1; 
+        else
+            r = m - 1; 
+    }  
     return -1; 
 }
 
 void busquedaBinaria(int *datos,int tam, int *datosB, int logs, int tamb) {
     for (size_t i = 0; i < tamb; i++)
     {
-        int encontrado =  buscar(datos, 0, tam, datosB[i]);
+        int encontrado = buscar(datos, 0, tam, datosB[i]);
         mostrar_informacion(encontrado, datosB[i], logs); 
     }
     
