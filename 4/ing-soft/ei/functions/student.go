8adfc7c2abe6d26534f0e4714ff2965dbd0cb061
Christian Hernandez Najera <chrismatla68@gmail.com>
2021-11-24 23:12:18 -0600

    [skip ci] I'll fix the build monday

--- 4/ing-soft/ei/functions/student.go
+++ 4/ing-soft/ei/functions/student.go
@@ -1,25 +1,49 @@
 package functions
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"time"
+)
+
+func LoginStudent(c echo.Context) error {
+	plate := c.Param("plate")
+	if plate == "" {
+		return c.String(http.StatusBadRequest, "Missing plate")
+	}
+
+	cookie := new(http.Cookie)
+	cookie.Name = "matricula"
+	cookie.Value = plate
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	c.SetCookie(cookie)
+	return c.String(http.StatusOK, "Ok")
+}
 
 func PassAsistence(c echo.Context) error {
 	return nil
 }
 
-func MiddlewareValidator(name, plate string, c echo.Context) (bool, error) {
-	cookie, err := c.Cookie("matricula")
-	if err != nil {
-		return false, err
-	}
+func ValidateStudent(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie("matricula")
+		if err != nil {
+			return err
+		}
 
-	if existPlate(cookie.Value) {
-		return true, nil
-	}
+		if !existPlate(cookie.Value) {
+			return c.String(http.StatusInternalServerError, "Plate doesn't exist")
+		}
 
-	return false, nil
+		return next(c)
+	}
+}
+
+func GetStudentsAsistence(c echo.Context) error {
+	return c.String(http.StatusOK, "")
 }
 
 func existPlate(plate string) (exist bool) {
-
+	exist = false
 	return
 }
--- 4/ing-soft/ei/server/server.go
+++ 4/ing-soft/ei/server/server.go
@@ -2,27 +2,28 @@ package server
 
 import (
 	"errors"
+	"github.com/labstack/echo/v4/middleware"
 	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"upemor.com/asistence/functions"
 )
 
 func StartServer() {
 	e := echo.New()
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	s := http.Server{
-		Addr:    "3000",
+		Addr:    ":3000",
 		Handler: e,
 	}
 
-	e.Use(middleware.BasicAuthWithConfig(middleware.BasicAuthConfig{
-		Validator: functions.MiddlewareValidator,
-	}))
-
-	e.POST("student/:plate", functions.PassAsistence)
+	e.POST("student/assistance/:plate", functions.PassAsistence, functions.ValidateStudent)
+	e.POST("student/:plate", functions.LoginStudent)
+	e.GET("students/asistence", functions.GetStudentsAsistence)
 
 	if err := s.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
