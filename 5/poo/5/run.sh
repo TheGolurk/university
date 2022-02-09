582b8d6e0a2924dc71d8f1488c41796e042ec991
Christian Hernandez Najera <chrismatla68@gmail.com>
2022-02-09 13:44:14 -0600

    Your commit is writing checks your merge can't cash.

---
+++ 5/poo/5/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+java -cp target/my-app-1.0-SNAPSHOT.jar:lib/javafaker-1.0.2.jar:lib/automaton-1.11-8.jar:lib/commons-lang3-3.5.jar:lib/generex-1.0.2.jar:lib/snakeyaml-1.23-android.jar org.example.Main
