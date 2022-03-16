10cdcb225e841404215bddbe2c2208b6e5d35343
Christian Hernandez Najera <chrismatla68@gmail.com>
2022-03-16 15:39:44 -0600

    GIT :/

---
+++ 5/poo/8/src/com/upemor/Prestamo.java
@@ -0,0 +1,9 @@
+package com.upemor;
+
+import java.util.Date;
+
+public class Prestamo {
+    private Date FechaPrestamo;
+    private Date FechaRegreso;
+    private Libro Libro;
+}
--- 5/poo/8/src/com/upemor/Usuario.java
+++ 5/poo/8/src/com/upemor/Usuario.java
@@ -1,4 +1,18 @@
 package com.upemor;
 
+import java.util.ArrayList;
+
 public class Usuario {
+    private int NumeroRegistro;
+    private String Nombre;
+    private String direccion;
+    private String telefono;
+    private ArrayList<Prestamo> LibrosPrestados;
+
+
+    public boolean SolicitarNuevoLibro(Libro libro) {
+        return true;
+    }
+
+
 }
