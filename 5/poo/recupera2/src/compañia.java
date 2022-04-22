fb80de97adda79a78745d6bdb1d481755b6a89aa
Christian Hernandez Najera <chrismatla68@gmail.com>
2022-04-22 15:04:28 -0500

    I'm too foo for this bar

--- 5/poo/recupera2/src/compañia.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serializable;
-
-public class compañia implements Serializable {
-    private sucursal sucursal;
-
-    public compañia(sucursal sucursal) {
-        this.sucursal = sucursal;
-    }
-}
