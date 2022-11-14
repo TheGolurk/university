353cc7115bfed4c8f03b4bdc528b5b2e3e760754
Christian Hernandez Najera <chrismatla68@gmail.com>
2022-11-14 15:17:23 -0600

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

---
+++ 7/len/ec2/1.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+void q0(char word[10], int index);
+void q1(char word[10], int index);
+
+int main(int argc, char const *argv[])
+{
+    char word[10];
+    printf("ingresa una cadena \n");
+    fgets(word, sizeof word, stdin);
+
+
+    q0(word, 0);
+
+    return 0;
+}
+
+void q0(char word[10], int index) {
+    if (index == 9) {
+        printf("El automata termina cadena valida \n");
+        return;
+    }
+    if (word[index] != 'a' && word[index] != 'b') {
+        printf("El automata termina cadena valida \n");
+        return;
+    }
+
+    if (word[index] == 'b') {
+        q0(word, index+1);
+    } else if (word[0] == 'a') {
+        q1(word, index+1);
+    } else {
+        printf("cadena con caracter %c invalida \n", word[index]);
+        return;
+    }
+}
+
+void q1(char word[10], int index) {
+    if(index == 9) {
+        printf("El automata no debe terminar en q1 y la cadena es invalida \n");
+        return;
+    }
+    if (word[index] != 'a' && word[index] != 'b') {
+        printf("El automata termina pero la cadena es invalida \n");
+        return;
+    }
+
+    if (word[index] == 'b') {
+        q1(word, index+1);
+    } else if (word[0] == 'a') {
+        q0(word, index+1);
+    } else {
+        printf("cadena con caracter %c invalida \n", word[index]);
+        return;
+    }
+}
