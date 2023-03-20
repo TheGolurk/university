448b76db97b5b5ae99a50ce6e935e0f299da3457
Christian Hernandez Najera <chrismatla68@gmail.com>
2023-03-20 11:54:20 -0600

    cambios

--- 8/IA/EP1/project/EJERCICIOS/ia/Examen/2.py
+++ 8/IA/EP1/project/EJERCICIOS/ia/Examen/2.py
@@ -1,5 +1,7 @@
 import random as rnd
-
+import random
+import math as mt
+import numpy as np
 
 def generarPoblacion():
   poblacion= np.zeros((8,6))
@@ -15,23 +17,70 @@ def torneo(poblacion,fitness):
     
     for i in range(8):
         ind1,ind2 = 0,0
-        while(ind1==ind2):
-            ind1 = rnd.randint()
-    
-    return []
+        while ind1==ind2:
+            ind1 = rnd.randint(0,5)
+            ind2 = rnd.randint(0,5)
+            
+        if (fitness[ind1]<fitness[ind2]):
+            pobTorneo.append(poblacion[ind1])
+        else:
+            pobTorneo.append(poblacion[ind2])
+    return pobTorneo
 
+
+def elitismo(poblacion, fitness, maximizar):
+    # Ordenar los fitness
+    fitness_ordenado = sorted(fitness, reverse=maximizar)
+    # Seleccionar los mejores cinco individuos
+    mejores_cinco = poblacion[fitness.index(fitness_ordenado[0]), :]
+    for i in range(1, 5):
+        mejores_cinco = np.vstack((mejores_cinco, poblacion[fitness.index(fitness_ordenado[i]), :]))
+    # Completar la población de 8 con tres individuos aleatorios de los cinco seleccionados
+    poblacion_elitista = mejores_cinco
+    for i in range(3):
+        poblacion_elitista = np.vstack((poblacion_elitista, mejores_cinco[rnd.randint(0, 4), :]))
+    return poblacion_elitista   
 
 def cruza_puntos(poblacion):
-    random.randint()
+    pobcruza=[]
+    x,x1=0,0
+
+    x=random.randint(0,7)
+    padre1 = poblacion[x]
+    x=random.randint(0,7)
+    padre2 = poblacion[x]
+    x=random.randint(0,7)
+    padre3 = poblacion[x]
     
-    return pobcruza[]
+    for i in range(8):
+        
+        for j in range(6):
+            # Punto inicial
+            x=random.randint(0,3)
+            # Punto final, debe ser mayor al punto inicial
+            x1 = -1
+            while x1 < x:
+                x1=random.randint(0,7)
+                
+
+            #una vez teniendo los dos puntos realizamos la cruza
+            
+
+    return pobcruza
 
 
 def muta_aleatorio(poblacion):
     return []
 
-def obtener_fitness():
-    return 0
+def obtener_fitness(poblacion):
+    fitness=[]
+    for i in range(8): #el recorrido de los individuos
+        f=0
+        for j in range(6): #recorrido de genes
+            f = 1/2((mt.pow(poblacion[i][j]),4)-16*(mt.pow(poblacion[i][j]),2)+5*(poblacion[i][j]))
+            fitness.append(f)
+
+    return fitness
 
 if __name__ == '__main__':
     generaciones = 200
@@ -40,7 +89,7 @@ if __name__ == '__main__':
     fitness = 1
 
     while(generaciones < n_gen  or fitness < fitlim):
-        poblacion = initPob()
+        poblacion = generarPoblacion()
         fitness = obtener_fitness(poblacion)
         poblacion = torneo(poblacion)
         poblacion = cruza_puntos(poblacion)
